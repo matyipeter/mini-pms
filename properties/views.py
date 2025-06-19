f2e0f8b41a7418d598793f251391d1d98e46b327
Mátyás Péter <peter@Matyass-MacBook-Pro.local>
2025-06-19 12:32:39 +0200

    cleaner code in properties/views.py and new functionality: update property. UI updates coming soon

--- properties/views.py
+++ properties/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import PropertyCreateForm
 from django.contrib.auth.decorators import login_required
 from .models import Property
@@ -7,111 +7,94 @@ from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 from django.views.generic.detail import DetailView
-from django.views.generic.list import ListView
-from users.models import OwnerProfile, TenantProfile, User
-from django.shortcuts import get_object_or_404
+from users.models import OwnerProfile, TenantProfile
 from datetime import date
 from lease.models import Lease
+from django.core.exceptions import PermissionDenied
 
-# Create your views here.
-
-
-class UpdatePropertyView(FormView, LoginRequiredMixin):
+class UpdatePropertyView(LoginRequiredMixin, FormView):
     form_class = PropertyCreateForm
     success_url = reverse_lazy('properties:success')
-    template_name = 'properties/update_property_form.html'
+    template_name = 'properties/owner/update_property_form.html'
 
+    def dispatch(self, request, *args, **kwargs):
+        if not self.request.user.is_owner:
+            raise PermissionDenied("You are not an owner.")
+        self.owner = OwnerProfile.objects.get(user=self.request.user)
+        property_id = self.kwargs['property_id']
+        try:
+            self.property = Property.objects.get(id=property_id, owner=self.owner)
+        except Property.DoesNotExist:
+            raise PermissionDenied("You do not have permission to edit this property.")
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['instance'] = self.property  # Prepopulate form with property data
+        return kwargs
 
     def form_valid(self, form):
-        if self.request.user.is_owner:
-            user = self.request.user
-            owner = OwnerProfile.objects.get(user=user)
-        else:
-            print("User is not owner")
-            owner = None
-        
-        property_id = self.kwargs['property_id']
-        owner.update_property(property_id, **form.cleaned_data)
+        # At this point, self.property is always the right property with the right owner
+        self.owner.update_property(self.property.id, **form.cleaned_data)
         return super().form_valid(form)
 
-
-
-class SuccessView(TemplateView, LoginRequiredMixin):
+class SuccessView(LoginRequiredMixin, TemplateView):
     template_name = 'properties/success.html'
 
-############
-
-class OwnerDashboardView(TemplateView):
+class OwnerDashboardView(LoginRequiredMixin, TemplateView):
     template_name = "properties/owner/owner_dashboard.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-
-        # Get the current owner
-        try:
-            owner = get_object_or_404(OwnerProfile, user=self.request.user)
-        except OwnerProfile.DoesNotExist:
-            print("Owner not found")
-            owner = None
-
-        # Get the properties of the logged-in owner
+        owner = get_object_or_404(OwnerProfile, user=self.request.user)
         properties = Property.objects.filter(owner=owner)
 
-        # Add context data for dashboard display
         context['properties'] = properties
         context['property_count'] = properties.count()
         context['owner'] = owner
-
         return context
 
-class TenantDashboardView(TemplateView):
+class TenantDashboardView(LoginRequiredMixin, TemplateView):
     template_name = "properties/tenant/tenant_dashboard.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         tenant = get_object_or_404(TenantProfile, user=self.request.user)
-
         current_lease = Lease.objects.filter(tenant=tenant, is_active=True).first()
         context["current_lease"] = current_lease
         context["property"] = current_lease.property if current_lease else None
         return context
 
-
-class PropertyDetailView(DetailView, LoginRequiredMixin):
+class PropertyDetailView(LoginRequiredMixin, DetailView):
     model = Property
     template_name = 'properties/owner/property_detail.html'
     context_object_name = 'property'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        today = date.today()
         current_lease = Lease.objects.filter(
             property=self.object,
         ).first()
         context["current_lease"] = current_lease
         return context
 
-
-class CreatePropertyView(CreateView, LoginRequiredMixin):
+class CreatePropertyView(LoginRequiredMixin, CreateView):
     model = Property
     form_class = PropertyCreateForm
     template_name = 'properties/owner/property_form.html'
 
     def form_valid(self, form):
-        if(self.request.user.is_owner):
+        if self.request.user.is_owner:
             owner = OwnerProfile.objects.get(user=self.request.user)
             form.instance.owner = owner
         else:
             print("User is not owner")
-            owner = None
-
         return super().form_valid(form)
 
     def get_success_url(self):
         return reverse_lazy("properties:owner_dashboard")
 
-
-class PropertyDeleteView(DeleteView):
+class PropertyDeleteView(LoginRequiredMixin, DeleteView):
     model = Property
     template_name = "properties/owner/property_delete.html"
     context_object_name = "property"
